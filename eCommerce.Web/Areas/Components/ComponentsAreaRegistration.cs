bdc5aa4a1c6bc4af1c678dc3678fc6c5d239b2b2
Nikolay Paskulov <npaskul@softserveinc.com>
2016-03-02 19:03:51 +0200

    Add Components Area in Web Project.

---
+++ eCommerce.Web/Areas/Components/ComponentsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace eCommerce.Web.Areas.Components
+{
+    public class ComponentsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Components";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Components_default",
+                "Components/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional, controller = "Components" }
+            );
+        }
+    }
+}
